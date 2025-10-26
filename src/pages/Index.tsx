d85f23c1f53e51d7882bbc6eb46590b0d0d9923f
frhn9204-creator <frhn9204@gmail.com>
2025-10-26 21:34:36 +0530

    Added central Hitler image to Index main page with theme-matching blur and opacity.

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,9 +3,25 @@ import { HeroSection } from '@/components/sections/HeroSection';
 
 const Index = () => {
   return (
-    <div className="relative">
-      <Scene3D />
-      <HeroSection />
+    <div className="relative min-h-screen bg-black">
+      {/* Central background image with blur and opacity */}
+      <div className="absolute inset-0 flex items-center justify-center">
+        <img
+          src="https://media.gettyimages.com/id/515381276/zh/%E7%85%A7%E7%89%87/adolf-hitler-closeup-shot-of-the-chancellor-speaking-over-the-radio-microphone.jpg?s=612x612&w=gi&k=20&c=JTD-yuGnQ2Dh2fNN6qtDgjwlXGOynwHRnPoMpNaGTSQ="
+          alt="Central themed image"
+          className="max-w-full max-h-full object-contain opacity-40 blur-sm"
+          style={{
+            filter: 'blur(2px)',
+            opacity: 0.4
+          }}
+        />
+      </div>
+      
+      {/* Existing components layered on top */}
+      <div className="relative z-10">
+        <Scene3D />
+        <HeroSection />
+      </div>
     </div>
   );
 };
